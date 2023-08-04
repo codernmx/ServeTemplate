4078a565ad7a03c7f1ee757cbdc91af10fbb8beb
lcl <liuchangling@linkon.me>
2023-08-04 15:53:19 +0800

    fix:config修改

--- config/index.js
+++ config/index.js
@@ -9,34 +9,30 @@ const dbPort = 3306  //数据库端口
 
 const port = 4000  //项目的端口
 
-const dbConfig = {
-	database, //数据库名
-	user, //用户名
-	password, //密码
-	options: {
-		host, //数据库服务器的IP地址或域名
-		port: dbPort, //数据库使用的端口号。MySQL数据库的默认端口号是3306
-		dialect: 'mysql',//数据库的类型
-		pool: { //数据库连接池：放若干个数据库的连接对象，提高数据库的访问效率
-			max: 20, //数据库连接池中连接对象的最大个数
-			min: 20, //数据库连接池中连接对象的最少个数
-			idle: 20000 //等待延迟的时间，单位是毫秒
-		},
-		logging: false, //是否开启 sql 日志
-		timezone: '+08:00',
-		define: {
-			timestamps: false, // 关键配置，默认为 true， 修改为 false 即可
-			'charset': 'utf8' //处理Mysql中中文字符问题
-		},
-		dialectOptions: {
-			dateStrings: true,
-			typeCast: true
-		}
+const options = {
+	host, //数据库服务器的IP地址或域名
+	port: dbPort, //数据库使用的端口号。MySQL数据库的默认端口号是3306
+	dialect: 'mysql',//数据库的类型
+	pool: { //数据库连接池：放若干个数据库的连接对象，提高数据库的访问效率
+		max: 20, //数据库连接池中连接对象的最大个数
+		min: 20, //数据库连接池中连接对象的最少个数
+		idle: 20000 //等待延迟的时间，单位是毫秒
+	},
+	logging: false, //是否开启 sql 日志
+	timezone: '+08:00',
+	define: {
+		timestamps: false, // 关键配置，默认为 true， 修改为 false 即可
+		'charset': 'utf8' //处理Mysql中中文字符问题
+	},
+	dialectOptions: {
+		dateStrings: true,
+		typeCast: true
 	}
 }
+
 module.exports = {
 	fileUrl,
-	dbConfig,
+	options,
 	database,
 	user,
 	password,
--- utils/sequelize.js
+++ utils/sequelize.js
@@ -1,8 +1,7 @@
+
 //1.导入Sequelize模块
 const Sequelize = require('sequelize')
-const { dbConfig } = require('../config/index')
-const { database, user, password, options } = dbConfig
-
+const { database, user, password, options } = require('../config/index')
 
 //new Sequelize('数据库名','用户名','密码',{配置信息})
 //2.使用sequelize模块配置和数据库的连接信息：创建连接数据库的对象
