e1d32ee49dec4f5f0f716a36164596cca3cba1ce
DESKTOP-1P21892 <2507128400@qq.com>
2024-02-19 11:50:00 +0800

    fix:日志按日期记录不丢失数据

--- utils/log4.js
+++ utils/log4.js
@@ -1,16 +1,19 @@
-const log4js = require ('log4js')
-const jwt = require ('../utils/jwt')
+const log4js = require('log4js')
+const jwt = require('../utils/jwt')
 
 //日志
 const logger = (name) => {
-	log4js.configure ({
+	log4js.configure({
 		appenders: {
 			out: { type: 'console' }, //在控制台输出日志
 			cheese: {
-				type: 'file',
-				filename: 'logs/log',
-				pattern: 'yyyy-MM-dd.log',
-				maxLogSize: 1024 * 1000 * 10 //10M
+				type: 'file', // 指定 appender 类型为 file
+				filename: 'logs/log', // 指定日志文件的基本名称（不包含日期部分）
+				pattern: 'yyyy-MM-dd.log', // 指定日期模式，以便生成带有日期的日志文件名
+				alwaysIncludePattern: true, // 始终包含日期模式在文件名中
+				keepFileExt: true, // 保留文件扩展名（即 .log）
+				backups: 100, // 保留10个日志文件备份
+				// maxLogSize: 1024 * 1000 * 10 //10M
 			}
 		},
 		categories: {
@@ -18,32 +21,32 @@ const logger = (name) => {
 			default: { appenders: ['cheese', 'out'], level: log4js.levels.DEBUG }
 		}
 	})
-	return log4js.getLogger (name)
+	return log4js.getLogger(name)
 }
 
 //添加日志
 const addFormatLog = function (req, res, data) {
-	const now = new Date ()
+	const now = new Date()
 	const resTime = now - req._startTime
 	let resTimeLevel = '01'
-	if ( resTime > 5000 ) {
+	if (resTime > 5000) {
 		resTimeLevel = '50'
-	} else if ( resTime > 2000 ) {
+	} else if (resTime > 2000) {
 		resTimeLevel = '20'
-	} else if ( resTime > 1000 ) {
+	} else if (resTime > 1000) {
 		resTimeLevel = '10'
-	} else if ( resTime > 500 ) {
+	} else if (resTime > 500) {
 		resTimeLevel = '05'
-	} else if ( resTime > 300 ) {
+	} else if (resTime > 300) {
 		resTimeLevel = '03'
-	} else if ( resTime > 200 ) {
+	} else if (resTime > 200) {
 		resTimeLevel = '02'
-	} else if ( resTime > 100 ) {
+	} else if (resTime > 100) {
 		resTimeLevel = '01'
 	} else {
 		resTimeLevel = '00'
 	}
-	const user = jwt.verifyToken (req.headers.token)
+	const user = req.headers.token ? jwt.verifyToken(req.headers.token) : {}
 	const {
 		ip,
 		headers,
@@ -68,21 +71,21 @@ const addFormatLog = function (req, res, data) {
 		userAgent: headers['user-agent'],
 		// data: data[0]
 	}
-	logger ('log').info (`${JSON.stringify (logInfo)}`)
+	logger('log').info(`${JSON.stringify(logInfo)}`)
 }
 
 //日志中间件
 const log4MiddleWare = () => {
 	return function (req, res, next) {
-		req._startTime = new Date ()
+		req._startTime = new Date()
 		const oldSend = res.send
 		res.send = function () {
-			oldSend.apply (res, arguments)
-			if ( typeof [...arguments][0] === 'object' ) {
-				res.once ('finish', () => addFormatLog (req, res, arguments))
+			oldSend.apply(res, arguments)
+			if (typeof [...arguments][0] === 'object') {
+				res.once('finish', () => addFormatLog(req, res, arguments))
 			}
 		}
-		return next ()
+		return next()
 	}
 }
 
